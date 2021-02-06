8ab047ba5c8b1a5980d3ed662c411890f4d9f05e
CrossChEp <61206698+CrossChEp@users.noreply.github.com>
2021-02-06 21:15:12 +0300

    Add files via upload

---
+++ binary_search/Arrs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Arrs
+    {
+
+        public static void Input(int[] arr)
+        {
+            for(int i = 0; i<arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+    }
+}
---
+++ binary_search/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            Arrs Arrs = new Arrs();
+
+            Console.WriteLine("Введите 10 чисел для заполнения");
+            int size = 5;
+            int[] arr = new int[size];
+
+            Arrs.Input(arr);
+
+
+            Array.Sort(arr);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("=============");
+            Arrs.Print(arr);
+            Console.WriteLine("=============");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            int Key;
+
+            
+
+            Console.WriteLine("Введите ключ");
+            Key = int.Parse(Console.ReadLine());
+
+            int Low = 0;
+            int Height = size - 1;
+
+            int mid;
+            mid = (Low + Height) / 2;
+            bool result = false;
+
+            while(Low <= Height)
+            {
+                
+
+                if(Key == arr[mid])
+                {
+                    result = true;
+                    break;
+                }
+
+                if(arr[mid] > Key)
+                {
+                    Height = mid - 1;
+                }    
+
+                else if(arr[mid] < Key)
+                {
+                    Low = mid + 1;
+                }
+            }
+
+
+
+            if(result == true)
+            {
+                Console.WriteLine(mid);
+            }
+            else
+            {
+                Console.WriteLine("None");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
